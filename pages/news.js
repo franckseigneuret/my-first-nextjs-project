02a220edc041769416f012f109a3143d4347b646
Franck Seigneuret <f.seigneuret@mazarine.com>
2022-01-28 15:12:53 +0100

    navigation nextJS

--- pages/news.js
+++ pages/news.js
@@ -1,10 +1,24 @@
 // our-domain.com/news
 
+import Link from "next/link"
+
 function News() {
   return (
-    <div>
-      news
-    </div>
+    <>
+      <h1>The news page</h1>
+      <ul>
+        <li>
+          <Link href="news/news-1">
+            News 1
+          </Link>
+        </li>
+        <li>
+          <Link href="news/news-2">
+            News 2
+          </Link>
+        </li>
+      </ul>
+    </>
   )
 }
 
